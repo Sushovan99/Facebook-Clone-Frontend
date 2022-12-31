44ab38668812a02def0c530ef8835e7d242514f2
Sushovan Biswas <biswassushovan99@gmail.com>
2022-12-31 21:17:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Friends } from "./svg";
+
+function App() {
+    return (
+        <>
+            <h2>welcome to frontend</h2>
+            <Friends color={"red"} />
+        </>
+    );
+}
+
+export default App;
